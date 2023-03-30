d5f94fbf19bcf0314ec5a9ee6afccdf1125be2ae
PavelKamenev <114611412+PavelKamenev@users.noreply.github.com>
2023-03-30 09:32:27 +0300

    Delete firstexercise.js

--- firstexercise.js
+++
@@ -1,4 +0,0 @@
-let firstNumber= Number(prompt("Type firstnumber ", "5"));
-let secondNumber = Number(prompt("Type secondnumber", "5"));
-let thirdNumber = Number(prompt("Type thirdnumber", "5"));
-console.log((firstNumber+secondNumber+thirdNumber)/3);
